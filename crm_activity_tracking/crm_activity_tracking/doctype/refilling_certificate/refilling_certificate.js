ef71f8d3ecebd77238e8bfe0a5aecf6aa823d091
Harshini <frappev15@ip-172-26-3-185.ap-south-1.compute.internal>
2024-12-02 09:49:07 +0000

    warranty

--- crm_activity_tracking/crm_activity_tracking/doctype/refilling_certificate/refilling_certificate.js
+++ crm_activity_tracking/crm_activity_tracking/doctype/refilling_certificate/refilling_certificate.js
@@ -1,8 +1,25 @@
 // Copyright (c) 2024, CRM and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Refilling Certificate", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Refilling Certificate", {
+	refresh(frm) {
+        frm.set_query("address", function (frm) {
+			return {
+			  filters: [
+				["Dynamic Link", "link_name", "=", frm.customer],
+				["Dynamic Link", "link_doctype", "=", "Customer"],
+			  ],
+			};
+		});
+	},
+    address(frm){
+        frm.set_query("address", function (frm) {
+			return {
+			  filters: [
+				["Dynamic Link", "link_name", "=", frm.customer],
+				["Dynamic Link", "link_doctype", "=", "Customer"],
+			  ],
+			};
+		});
+    }
+});
