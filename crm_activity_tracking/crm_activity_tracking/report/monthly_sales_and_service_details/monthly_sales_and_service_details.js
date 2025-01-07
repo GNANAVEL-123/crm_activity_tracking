cfa3dcd109e1bb237e38ef752fe536d88e127238
Harshini <frappev15@ip-172-26-3-185.ap-south-1.compute.internal>
2025-01-07 08:17:32 +0000

    service_details

---
+++ crm_activity_tracking/crm_activity_tracking/report/monthly_sales_and_service_details/monthly_sales_and_service_details.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, CRM and contributors
+// For license information, please see license.txt
+
+frappe.query_reports["Monthly Sales and Service Details"] = {
+	"filters": [
+
+	]
+};
