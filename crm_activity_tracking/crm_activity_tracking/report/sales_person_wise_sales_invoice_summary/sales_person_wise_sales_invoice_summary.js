918fb79599e1b540f675eabea2a21ad058043b7b
Harshini <frappev15@ip-172-26-3-185.ap-south-1.compute.internal>
2025-12-30 06:01:09 +0000

    sales and admin

---
+++ crm_activity_tracking/crm_activity_tracking/report/sales_person_wise_sales_invoice_summary/sales_person_wise_sales_invoice_summary.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, CRM and contributors
+// For license information, please see license.txt
+
+frappe.query_reports["Sales Person Wise Sales Invoice Summary"] = {
+	"filters": [
+
+	]
+};
